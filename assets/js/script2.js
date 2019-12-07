38c6874646431aa9e1fdb4db2118e2a0406a2f66
Chandu Koritala <chanduk0406@gmail.com>
2019-12-07 13:25:26 +1030

    changed image sizes

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,27 +1,41 @@
-var content = $("#inputText").val();
-var APIKey = "14518866-15b065df23eaa04c7d4ef76d0";
 
-$.ajax({
-    url: "https://pixabay.com/api/?key="+APIKey+"&q="+content+"&image_type=photo",
-    method: "GET"
-})
+$(document).ready(function(){
 
-.then(function(response){
+    $("#searchBtn").on("click", function(){
+        $("#foodPic").empty();
+        var content = $("#inputText").val();
+        var APIKey = "14518866-15b065df23eaa04c7d4ef76d0";
+    
+        $.ajax({
+            url: "https://pixabay.com/api/?key="+APIKey+"&q="+content+"&image_type=photo",
+            method: "GET"
+        })
+        
+        .then(function(response){
+        
+            console.log(response);
+        
+            var results = response.hits[0].webformatURL;
+        
+            var imgFood = $("<img>");
+        
+            imgFood.attr("src", results);
+            
+            imgFood.attr("style", "height: 500px; width: 600px;");
+        
+        
+            console.log(results);
+        
+            // appending the img div 
+        
+            $("#foodPic").append(imgFood);
+        })
+            
+    
+    });
+    
+    
 
-    console.log(response);
-
-    var results = response.hits[0].largeImageURL;
-
-    var imgFood = $("<img>");
-
-    imgFood.attr("src", results);
-
-    console.log(results);
-
-    // appending the img div 
-
-    $("#foodPic").append(imgFood);
-})
-
+});
 
 
