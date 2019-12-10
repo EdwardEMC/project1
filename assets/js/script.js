2c2ffe77f934be25b81998bb79fdfafca588201c
EMC <edwardcoad.ec@gmail.com>
2019-12-10 19:35:17 +1030

    buttons fixed, search for ingredient instead of meal to ease recipe selection problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,53 +3,97 @@ $(document).ready(function(){
     var recipeTitle = $("#recipeTitle");
     var y;
 
-    //Checks local storage and sets y to either 0 or the id of the previous button
-    checkStorage();
+    checkStorage(); //Checks local storage and sets y to either 0 or the id of the previous button
 
+    //on click on the search input to search for an ingredient
     $("#searchBtn").on("click", function(){
         var content = $("#inputText").val();
         clearAll(); //emptying the fields
-        request(content);
-        buttonCreate(content);
+        var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i="+content;
+        request(content, queryURL);
     });   
 
+    //on click to search the saved search buttons
     $(".addButtons").on("click", function(event){
         event.preventDefault();
         var content = event.target.value;
+        var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+content;
         clearAll();
-        request(content);
+        request(content, queryURL);
+    });
+
+    //on click to search one of the recipes added
+    recipeArea.on("click", function(event){
+        event.preventDefault();
+        var content = event.target.value;
+        var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+content;
+        request(content, queryURL);
+        clearAll();
     });
     
-    function request(content){
+    //function containing the ajax request
+    function request(content, queryURL){
         $.ajax({
-        url:"https://www.themealdb.com/api/json/v1/1/search.php?s="+content,
+        url: queryURL,
         METHOD: "GET"
         }).then(function(response){
             console.log(response);
-            var ingredients = [];
-            var i = 1;
-            while(response.meals[0]["strIngredient"+i]!=="") {
-                ingredients.push(response.meals[0]["strIngredient"+i]);
-                i++;
+            var i = 0; //reset the counter for every request
+
+            if(queryURL=="https://www.themealdb.com/api/json/v1/1/filter.php?i="+content) {
+                var meals = [];
+                while(i<response.meals.length) {
+                    meals.push(response.meals[i].strMeal);
+                    i++; 
+                }
+                for(x=0; x<meals.length; x++) {
+                    var button = $("<button>").text(meals[x]);
+                    button.val(meals[x]);
+                    button.attr("id", y);
+                    button.addClass("btn btn-info"); //change to bulma css---------------------------------------------------------------------
+                    recipeArea.append(button);
+                }
             }
-            
-            recipeName(response);
-            ingrList(ingredients);
-            method(response);
+            else {
+                displayRecipe(response, content);
+            }   
+        }).fail(function() { //Adding a function if the ajax request fails
+            recipeArea.text("There is no recipe available with that name.");
+            return;
         });
+    }
+
+    //function to display recipe
+    function displayRecipe(response, content) {   
+        var ingredients = [];
+        var i = 1;
+        while(response.meals[0]["strIngredient"+i]!=="") {
+            ingredients.push(response.meals[0]["strIngredient"+i]);
+            i++;
+        }
+        buttonCreate(content);
+        recipeName(response);
+        ingrList(ingredients);
+        method(response);
     }
 
     //function to create the buttons
     function buttonCreate(content) {
-        var button = $("<button>").text(content);
-        button.val(content);
-        button.text(content);
-        button.attr("id", y);
-        button.addClass("btn btn-info");
-        $(".addButtons").append(button);
-        localStorage.setItem(y, content); //saving the recipe searched to local storage
-        y = y + 1;
-        localStorage.setItem("tracker", y);
+        var alreadyButtons = [];
+        for(x=0; x<y; x++) {
+            alreadyButtons.push(localStorage.getItem(x));
+        }
+        //if condition to check if the button is already there
+        if(!alreadyButtons.includes(content)) {
+            var button = $("<button>").text(content);
+            button.val(content);
+            button.attr("id", y);
+            button.addClass("btn btn-info"); //change to bulma css---------------------------------------------------------------------
+            $(".addButtons").append(button);
+            localStorage.setItem(y, content); //saving the recipe searched to local storage
+            y = y + 1;
+            localStorage.setItem("tracker", y); 
+        }
     }
     //function to clear divs after each search
     function clearAll(){
